e9968e3f24899d72f3efa7e72627d91b8a308684
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-30 15:01:18 -0600

    Add the react app link to config.xml

--- platforms/ios/www/scripts/index.js
+++ platforms/ios/www/scripts/index.js
@@ -12,7 +12,7 @@ var app = {
         StatusBar.backgroundColorByName('black');
 
         // Here, we redirect to the web site.
-        var targetUrl = "https://katy-hiking-trails.herokuapp.com/";
+        var targetUrl = "https://katy-hiking-trails-react.herokuapp.com/";
         var bkpLink = document.getElementById("bkpLink");
         bkpLink.setAttribute("href", targetUrl);
         bkpLink.text = targetUrl;
